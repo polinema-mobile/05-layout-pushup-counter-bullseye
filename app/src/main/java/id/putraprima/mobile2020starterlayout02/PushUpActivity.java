6d587f60552f706de9682b2133776cc5feb33bb1
Unero <20809454+Unero@users.noreply.github.com>
2020-09-15 15:43:46 +0700

    No 13 a,b,c,d

--- app/src/main/java/id/putraprima/mobile2020starterlayout02/PushUpActivity.java
+++ app/src/main/java/id/putraprima/mobile2020starterlayout02/PushUpActivity.java
@@ -1,14 +1,34 @@
 package id.putraprima.mobile2020starterlayout02;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class PushUpActivity extends AppCompatActivity {
+
+    private int counterPush = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_push_up);
+        // Mengeset nilai counter ke 0 saat activity;
+        TextView textcounter = (TextView) findViewById(R.id.txtCounter);
+        textcounter.setText("0");
+    }
+
+    public void toMainMenu(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void pushClick(View view){
+        counterPush += 1;
+        TextView textcounter = (TextView) findViewById(R.id.txtCounter);
+        textcounter.setText(""+counterPush);
     }
 }
